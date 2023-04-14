e8fa66026aab43df8ca4589847ebcb92eb8fb6c8
Hana-Atef <94763745+Hana-Atef@users.noreply.github.com>
2023-04-14 14:49:02 +0200

    Update models.py

--- project/models.py
+++ project/models.py
@@ -1,12 +1,2 @@
 from django.db import models
 
-
-# class Try(models.Model):
-
-#   username = models.CharField(max_length=15)
-#   email = models.EmailField(max_length=15)
-#   password = models.CharField(max_length=8)
-#   re_password = models.CharField(max_length=8)
-  
-#   class Meta:
-#       db_table = ''
